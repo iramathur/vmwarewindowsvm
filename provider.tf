cf7fbf55d280ab77766769582f3abd449883ff93
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-08-17 17:52:12 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantId
+  #version        = "=2.0.0"
+  features {}
+}
