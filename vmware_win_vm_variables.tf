b584a4ec19580928c4e3fc0c8f0bfd0c2ccfaabb
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-08-17 17:50:31 +0530

    Update vmware_win_vm_variables.tf

--- vmware_win_vm_variables.tf
+++ vmware_win_vm_variables.tf
@@ -1,47 +1,105 @@
-variable "user" {
-    type = "string"
+variable "subscriptionId" {
 }
-variable "password" {
-    type = "string"
+
+variable "clientId" {
 }
-variable "host" {
-    type = "string"
+
+variable "clientSecret" {
 }
+
+variable "tenantId" {
+}
+
+variable "resource_group" {
+  description = "The name of the resource group in which to create the virtual network."
+  default     = "terraform-group"
+}
+
+variable "rg_prefix" {
+  description = "The shortened abbreviation to represent your resource group that will go on the front of some resources."
+  default     = "rg"
+}
+
+variable "hostname" {
+  description = "VM name referenced also in storage-related names."
+  default     = "tf"
+}
+
+variable "dns_name" {
+  description = " Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
+  default     = "vm"
+}
+
+variable "lb_ip_dns_name" {
+  description = "DNS for Load Balancer IP"
+  default     = "lb1"
+}
+
+variable "vm_count_per_subnet" {
+  description = "count per subnet"
+  default     = 2
+}
+
 variable "region" {
-    type = "string"
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
 }
-variable "datastore" {
-    type = "string"
-    default = "vmstore"
+
+variable "virtual_network_name" {
+  description = "The name for the virtual network."
+  default     = "vnet"
 }
-variable "cluster" {
-    type = "string"
-    default = "fenrir"
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
 }
-variable "network_interface" {
-    type = "string"
-    default = "portGroup-1004"
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
 }
-variable "templateName" {
-    type = "string"
-    default = "windows2012"
+
+variable "storage_account_tier" {
+  description = "Defines the Tier of storage account to be created. Valid options are Standard and Premium."
+  default     = "Standard"
 }
-variable "offset" {
-    default = 1
+
+variable "storage_replication_type" {
+  description = "Defines the Replication Type to use for this storage account. Valid options include LRS, GRS etc."
+  default     = "LRS"
 }
-variable "count" {
-    default = 2
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_D1_v2"
 }
-variable "cpucount" {
-    type = "string"
+
+variable "image_publisher" {
+  description = "name of the publisher of the image (az vm image list)"
+  default     = "MicrosoftWindowsServer"
 }
-variable "memsize" {
-    type = "string"
+
+variable "image_offer" {
+  description = "the name of the offer (az vm image list)"
+  default     = "WindowsServer"
 }
-variable "foldername" {
-    type = "string"
-    default = "dev_zone"
+
+variable "image_sku" {
+  description = "image sku to apply (az vm image list)"
+  default     = "2012-R2-Datacenter"
 }
-variable "vmname" {
-    type = "string"
+
+variable "image_version" {
+  description = "version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+
+variable "admin_username" {
+  description = "administrator user name"
+  default     = "vmadmin"
+}
+
+variable "admin_password" {
+  description = "administrator password (recommended to disable password auth)"
+  default     = "admin02!"
 }
