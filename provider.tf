5b7dc419dacec69e4035821be6512ba2a5906b49
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-08-17 18:49:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
-  subscription_id = var.subId
-  client_id       = var.clinId
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
   client_secret   = var.clientSecret
   tenant_id       = var.tenantId
   #version        = "=2.0.0"
